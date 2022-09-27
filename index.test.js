2f4d0ddb30ce77ad5ff5b23645d14bfd95d2e225
falak Chandni <falak.chandni@codingninjas.com>
2022-09-27 15:30:01 +0530

    babel preset import

--- index.test.js
+++ index.test.js
@@ -1,5 +1,7 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import "@babel/preset-react"
+import "@babel/plugin-syntax-jsx"
 
 import App from "./index";
 
