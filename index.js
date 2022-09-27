4912643b2136398290f2de33ce05ac68bd2cad48
falak Chandni <falak.chandni@codingninjas.com>
2022-09-27 13:32:30 +0530

    index.js and index.html added

---
+++ index.js
@@ -0,0 +1,15 @@
+function App() {
+    return (
+      <>
+        <h1>Student</h1>
+        <h4> This is a simple project made using React </h4>
+        <p >Skills usbh to make this project</p>
+        <ol >
+          <li>HTML</li>
+          <li>javaScript</li>
+          <li>React</li>
+        </ol>
+      </>
+    );
+  }
+ReactDOM.createRoot(document.getElementById("root")).render(<App />); 
